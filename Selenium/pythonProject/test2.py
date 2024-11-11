cfc9aadf7d50bca5556dcd5c11d0f460ec433d47
RubinoPD <robertaskask@gmail.com>
2024-11-11 10:21:00 +0200

    Add Selenium imports and config to test2.py

---
+++ Selenium/pythonProject/test2.py
@@ -0,0 +1,19 @@
+import os.path
+from sys import executable, exception
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+import time
+
+options = Options()
+options.binary_location = "C:/Program Files/Mozilla Firefox/firefox.exe"
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+
